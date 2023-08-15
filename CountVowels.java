856cbb12e6353c4a6f7f6f678e0f5494e9bb4cbb
PraveenSarangan01 <142241221+PraveenSarangan01@users.noreply.github.com>
2023-08-16 00:20:41 +0530

    Add files via upload
    FirstCommit Vowels Added

---
+++ CountVowels.java
@@ -0,0 +1,25 @@
+public class CountVowels {
+
+	public static void main(String[] args) {
+		String str = "Praveen";
+		int vowelscount=0;
+		int consonants =0;
+		
+		for(int i =0; i<str.length();i++) {
+			char ch = str.charAt(i);
+			
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+				vowelscount++;
+				
+			}
+			else {
+				consonants++;
+			}
+		}
+		System.out.println("Vowels count is :"+vowelscount);
+		System.out.println("Consonants count is :"+consonants);
+		
+
+	}
+
+}
